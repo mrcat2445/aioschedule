3d5d9ed2a363f5e736d0d27559d5576b3ea0878d
mrcat2445 <misazver999@gmail.com>
2025-08-05 14:36:44 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='aioschedule',  # Укажите уникальное имя для вашего пакета
     version='0.1.0',            # Версия вашего пакета
-    py_modules=['aioschedule.py'], # Указываем, что это один модуль из файла aioschedule.py
+    py_modules=['aioschedule'], # Указываем, что это один модуль из файла aioschedule.py
     description='Modified aioschedule for Python 3.10+',
     long_description=open('README.md', encoding='utf-8').read() if 'README.md' else '',
     long_description_content_type='text/markdown',
