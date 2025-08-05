cf4843047089d69a63d27f8967a51df2f803163f
mrcat2445 <misazver999@gmail.com>
2025-08-05 14:21:43 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='aioschedule',  # Укажите уникальное имя для вашего пакета
+    version='0.1.0',            # Версия вашего пакета
+    py_modules=['__init__.py'], # Указываем, что это один модуль из файла aioschedule.py
+    description='Modified aioschedule for Python 3.10+',
+    long_description=open('README.md', encoding='utf-8').read() if 'README.md' else '',
+    long_description_content_type='text/markdown',
+    author='mrcat2445',
+    author_email='mrcat2445official@gmail.com',
+    url='https://github.com/mrcat2445/aioschedule',
+    install_requires=[
+        # Если ваш aioschedule.py использует какие-либо внешние библиотеки,
+        # перечислите их здесь. Например:
+        # 'asyncio', # Обычно asyncio встроен, но если есть другие...
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
